48288181eaaed0f61652785df3583396461c50cd
Matt Maslen <mattmaslen123@gmail.com>
2020-12-09 16:00:34 +0000

    split app.js into components and added use of props

--- src/App.js
+++ src/App.js
@@ -1,3 +1,8 @@
+import Header from "./components/Header";
+import ResetButton from "./components/ResetButton";
+import Score from "./components/Score";
+import Winner from "./components/Winner";
+
 const App = ({
 	won,
 	p1Serving,
@@ -8,67 +13,27 @@ const App = ({
 	onReset,
 }) => (
 	<>
-		{/* header */}
-		<header className="jumbotron mt-4 mb-0">
-			<h1>PongPing</h1>
-		</header>
-
-		{/* scores */}
+		<Header />
+		{/* className on div here keeps Score rendered ui in row */}
 		<div className="row mb-4">
-			<div className="col-md-6 mt-4">
-				<div
-					className={`card text-center ${
-						p1Serving ? "bg-dark text-white" : ""
-					}`}
-				>
-					<h5 className="card-header">Player 1</h5>
-					<div className="card-body">
-						<p className="card-text display-1">{player1Score}</p>
-					</div>
-					<div className="card-footer">
-						<button
-							className="form-control btn btn-success"
-							onClick={onIncrementplayer1}
-						>
-							+
-						</button>
-					</div>
-				</div>
-			</div>
-
-			<div className="col-md-6 mt-4">
-				<div
-					className={`card text-center ${
-						p1Serving ? "" : "bg-dark text-white"
-					}`}
-				>
-					<h5 className="card-header">Player 2</h5>
-					<div className="card-body">
-						<p className="card-text display-1">{player2Score}</p>
-					</div>
-					<div className="card-footer">
-						<button
-							className="form-control btn btn-success"
-							onClick={onIncrementplayer2}
-						>
-							+
-						</button>
-					</div>
-				</div>
-			</div>
+			{" "}
+			{/* passing props into <Score /> */}
+			<Score
+				player={"Player1"}
+				handleClickPlayerScore={onIncrementplayer1}
+				currentScore={player1Score}
+				server={p1Serving}
+			/>
+			<Score
+				player={"Player2"}
+				handleClickPlayerScore={onIncrementplayer2}
+				currentScore={player2Score}
+				server={!p1Serving}
+			/>
 		</div>
-
-		{/* winner message */}
-		{won ? (
-			<h2 className="alert alert-success">{`Player ${won} wins!`}</h2>
-		) : null}
-
+		<Winner winner={won} />
 		<hr />
-
-		{/* reset button */}
-		<button className="btn btn-danger" onClick={onReset}>
-			Reset
-		</button>
+		<ResetButton handleReset={onReset} />
 	</>
 );
 
---
+++ src/components/Score.js
@@ -0,0 +1,22 @@
+const Score = ({ server, player, currentScore, handleClickPlayerScore }) => (
+	<>
+		<div className="col-md-6 mt-4">
+			<div className={`card text-center ${server ? "bg-dark text-white" : ""}`}>
+				<h5 className="card-header">{player}</h5>
+				<div className="card-body">
+					<p className="card-text display-1">{currentScore}</p>
+				</div>
+				<div className="card-footer">
+					<button
+						className="form-control btn btn-success"
+						onClick={handleClickPlayerScore}
+					>
+						+
+					</button>
+				</div>
+			</div>
+		</div>
+	</>
+);
+
+export default Score;
